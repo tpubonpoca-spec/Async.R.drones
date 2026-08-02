8b454cdd6e193a9e64a6c0802901167dabbd276c
TpuBonpoca <tpubonpoca@gmail.com>
2026-08-02 09:46:48 +0300

    Убрал лишние меши рук из модели, отцентровал геймпад и привязал посадочное место прямо в hands C_Hands

--- zAsync/lua/weapons/weapon_async_gamepad.lua
+++ zAsync/lua/weapons/weapon_async_gamepad.lua
@@ -39,11 +39,11 @@ if CLIENT then
     CreateClientConVar("async_gamepad_ang_y", "-180", true, false, "Угол рыскания (Yaw)")
     CreateClientConVar("async_gamepad_ang_r", "-180", true, false, "Угол крена (Roll)")
 
-    -- Точная подстройка экрана смартфона на модели BESTheldinarms
-    CreateClientConVar("async_gamepad_screen_x", "-16.1", true, false, "Смещение экрана по X")
-    CreateClientConVar("async_gamepad_screen_y", "-0.5", true, false, "Смещение экрана по Y")
-    CreateClientConVar("async_gamepad_screen_z", "1.5", true, false, "Смещение экрана по Z")
-    CreateClientConVar("async_gamepad_screen_scale", "0.026", true, false, "Масштаб 3D2D экрана")
+    -- Точная подстройка чистой модели геймпада ровно в руки
+    CreateClientConVar("async_gamepad_screen_x", "0.0", true, false, "Смещение экрана по X")
+    CreateClientConVar("async_gamepad_screen_y", "0.0", true, false, "Смещение экрана по Y")
+    CreateClientConVar("async_gamepad_screen_z", "2.0", true, false, "Смещение экрана по Z")
+    CreateClientConVar("async_gamepad_screen_scale", "0.025", true, false, "Масштаб 3D2D экрана")
 end
 
 SWEP.Weight = 0
